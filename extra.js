f4a3a8880935913f02d3b7949e570b12a185f209
Rodolphe Lepigre <lepigre@mpi-sws.org>
2020-12-03 02:58:30 +0100

    Inital commit for the webpage.

---
+++ extra.js
@@ -0,0 +1,43 @@
+$('body').scrollspy({
+   offset: $(window).height() * 0.4
+});
+
+$(window).on('scroll', function() {
+  if ($(this).scrollTop() > 200) {
+    $('.navbar-brand').addClass('small-brand')
+  } else {
+    $('.navbar-brand').removeClass('small-brand')
+  }
+});
+
+// The following was borrowed on "css-tricks.com".
+$('a[href^="#"]:not([href^="#collapse"])').click(function(event) {
+  // On-page links
+  if (location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') 
+      && location.hostname == this.hostname) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name='+this.hash.slice(1)+']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top - 70
+      }, 400, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+
